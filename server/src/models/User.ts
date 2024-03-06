9b969502037dff5b80f1a81f51718ff6bdfefe2e
Ritu <singh.ritu.rajiv@gmail.com>
2024-03-06 20:38:56 +0530

    fix : mongodb validation error

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -5,8 +5,8 @@ import { UserType } from '../../shared/types';
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  fName: { type: String, required: true },
+  lName: { type: String, required: true },
 });
 
 userSchema.pre("save", async function (next : any) {
